af1e5026804ff20313eac724a85b3f344417a5c6
Abdul R. Wahab <arwahab@users.noreply.github.com>
2018-06-17 17:02:56 -0400

    Add files via upload

---
+++ src/SubstringMethodDemo.java
@@ -0,0 +1,7 @@
+public class SubstringMethodDemo {
+	public static void main(String args[]) {
+		String s = "TonyPaadaalaankaaDang";
+		System.out.println(s.substring(6));
+		System.out.println(s.substring(0, 6));
+	}
+}
